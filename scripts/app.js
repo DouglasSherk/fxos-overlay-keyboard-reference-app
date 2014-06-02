a16d1882e13df1760333821450571fd441a0d434
DouglasSherk <github@sherk.me>
2014-06-02 16:49:25 +0800

    Add numbers.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,11 @@
 window.onload = function() {
   var content = document.getElementById('content');
   content.style.height = (window.innerHeight - 100) + "px";
+
+  var contentInner = document.getElementById('content-inner');
+  var innerHTML = '';
+  for (var i = 1; i <= 200; i++) {
+    innerHTML += i + '<br>';
+  }
+  contentInner.innerHTML += innerHTML;
 };
